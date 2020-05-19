a1dd7ed406eb4a239eb9fa7447e49bd947803078
soheel <soheelsaig0966@gmail.com>
2020-05-20 01:56:14 +0200

    mag

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $ (document).ready (function () {
       let counter = parseInt (document.getElementById ('timer').textContent);
       document.getElementById ('timer').textContent = counter - 1;
       if (counter > 1) timerId = setTimeout (tick, 1000);
-      $ ('.firstCard').fadeOut (9000, function () {
+      $ ('.firstCard').fadeOut (10000, function () {
         $ ('.secondCard').fadeIn (3000, function () {
           $ ('.find').html ('The card you chose has disappeared');
           $ ('.restart').fadeIn ();
